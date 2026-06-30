f2e63d3ac998828737af7b62818fdcfe9fe4fc8c
manicharan-ship-it <gummadirajumanicharan@gmail.com>
2026-06-30 18:24:56 +0530

    Refactor clock.js to remove commented code

--- barabari/dom/clock/clock.js
+++ barabari/dom/clock/clock.js
@@ -35,26 +35,3 @@ dateElement.textContent= `${day}/${month}/${year.toString().padStart(2,"0")}`;
 }
 updatedate();
 setInterval(updatedate,1000);
-// function updatetime(){
-//     let now = new Date();
-//     let hour = now.getHours();
-//     const minutes = now.getMinutes().toString().padStart(2 , "0");
-//     const seconds = now.getSeconds().toString().padStart(2 , "0");
-//     timeElement.textContent = `${hour}:${minutes}:${seconds}`;
-// if(is24format){
-//     timeElement.textContent = `${hour.toString().padStart(2,"0")}:${minutes}:${seconds}`;
-// }else{
-//     const ampm = hour >= 12 ? "pm ":"am";
-//     hour = hour%12||12;
-//     timeElement.append(ampm);
-
-//      timeElement.textContent = `${hour.toString().padStart(2,"0")}:${minutes}:${seconds}:${ampm}`;
-
-// }
-//     toggleBtn.addEventListener( "click", () => {
-//     is24format=!is24format;
-//     toggleBtn.textContent = is24format ? "switch to 12 hours format":" switch to 24 hours format ";
-//     });       
-// }
-// updatetime();
-// setInterval(updatetime,1000);
